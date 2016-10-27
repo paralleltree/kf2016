7ec6bf8303ed59cba7857d37a7a851c68a45a7c2
Paralleltree <paralleltree@outlook.com>
2016-10-27 21:21:11 +0900

    Add server.rb

---
+++ server.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require './models/loader.rb'
+require_relative 'streaming'
+
+get '/fetch' do
+  res = Status.where("id > ?", params['max_status_id'].to_i).order(:id).limit(3)
+  res.to_json(include: { user: { only: [:screen_name, :name, :profile_image_url] }, media: { only: [:url] } }, except: [:user_id, :created_at, :updated_at])
+end
