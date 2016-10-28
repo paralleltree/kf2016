7d41180d4d7c1057e69f78b982287b88ab1ee57d
Paralleltree <paralleltree@outlook.com>
2016-10-28 10:09:57 +0900

    Extract creating records and add rest api

--- streaming.rb
+++ streaming.rb
@@ -10,17 +10,30 @@ token = {
 
 @rest = Twitter::REST::Client.new(token)
 @stream = Twitter::Streaming::Client.new(token)
+words = %w(#Koryosai2016 #Koryosai #工陵祭)
+
+def parse(st)
+  user = User.new(id: st.user.id, screen_name: st.user.screen_name, name: st.user.name, profile_image_url: st.user.profile_image_url_https).tap { |u| u.save }
+  status = Status.new(id: st.id, user_id: st.user.id, text: st.text, url: st.url).tap { |s| s.save }
+  st.media.each do |m|
+    Medium.new(id: m.id, url: m.media_url, status_id: st.id).save
+  end
+  true
+rescue => e
+  false
+end
+
+@rest.search(words.join(" "), result_type: :recent, count: 100, exclude: :retweets, since_id: Status.maximum(:id)).to_a.each do |st|
+  next unless st.media.count > 0
+  parse(st)
+end
 
 Thread.new do
-  @stream.filter(track: "#Kouryo2016,#koryosai,#工陵祭") do |st|
+  @stream.filter(track: words.join(","), exclude: :retweets) do |st|
     case st
     when Twitter::Tweet
       next unless st.media.count > 0
-      user = User.new(id: st.user.id, screen_name: st.user.screen_name, name: st.user.name, profile_image_url: st.user.profile_image_url_https).tap { |u| u.save }
-      status = Status.new(id: st.id, user_id: st.user.id, text: st.text, url: st.url).tap { |s| s.save }
-      st.media.each do |m|
-        Medium.new(id: m.id, url: m.media_url, status_id: st.id).save
-      end
+      parse(st)
     end
   end
 end
